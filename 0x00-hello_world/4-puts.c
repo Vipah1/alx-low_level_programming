4402249c0f702434d6c627ead0c0fe80d0d79c16
Osman <oselibas@gmail.com>
2023-02-16 13:09:20 -0800

    4- puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -A program that printts a line with puts function.
+ * Return: 0 if success
+*/
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle\n");
+  return (0);
+}
