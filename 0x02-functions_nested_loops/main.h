65f0843d9c2c0326a2106f0a491d30430f366d8a
Osman <oselibas@gmail.com>
2023-02-21 15:54:12 -0800

    char

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,4 +5,18 @@
 #ifndef MAIN_T
 #define MAIN_T
 
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_baver(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void peint_times_table(int n);
+
 #endif
