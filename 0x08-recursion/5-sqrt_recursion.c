8880f611d9e94902543c3033ad4e90a1fe3a765d
Osman <oselibas@gmail.com>
2023-03-08 14:34:06 -0800

    square

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-int act_sqrt_recursion(int n, int i);
+int actual_sqrt_recursion(int n, int i);
 /**
  * _sqrt_recursion - returns the square root of a number.
  * @n: the number to be square rooted.
@@ -11,5 +11,5 @@ if (n < 0)
 {
 return (-1);
 }
-return (act_sqrt_recursion(n,0));
+return (actual_sqrt_recursion(n,0));
 }
