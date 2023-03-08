c39b043a6fffca961b970fad60e1fe9d4f7299ff
Osman <oselibas@gmail.com>
2023-03-08 14:36:21 -0800

    hwee root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-int actual_sqrt_recursion(int n, int i);
 /**
  * _sqrt_recursion - returns the square root of a number.
  * @n: the number to be square rooted.
@@ -11,5 +10,5 @@ if (n < 0)
 {
 return (-1);
 }
-return (actual_sqrt_recursion(n,0));
+return (_sqrt_recursion(n));
 }
