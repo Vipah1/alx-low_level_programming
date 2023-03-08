762f6d3dcee5819b1d8dfab244f21c39d52655be
Osman <oselibas@gmail.com>
2023-03-08 14:27:00 -0800

    root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns the square root of a number.
+ * @n: the number to be square rooted.
+ * Return: the resulting square root
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+return (_sqrt_recursion(n), 0);
+}
