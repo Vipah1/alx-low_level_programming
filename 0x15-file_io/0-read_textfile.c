45ca2887c4e04072cd9951249b6f6f879708af23
Osman <oselibas@gmail.com>
2023-04-08 09:48:58 -0700

    reading letters correccted

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,13 +14,13 @@ if (!filename)
 {
 return (0);
 }
-fd = open(filename, 0_RDONLY);
+fd = open(filename, O_RDONLY);
 if (fd == -1)
 {
 return (0);
 }
 buf = malloc(sizeof(char) * (letters));
-if (!buff)
+if (!buf)
 {
 return (0);
 }
