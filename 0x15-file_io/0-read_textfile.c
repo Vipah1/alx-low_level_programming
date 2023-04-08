30b4ac574820ff0fc32bfff572054e415b765482
Osman <oselibas@gmail.com>
2023-04-08 09:43:38 -0700

    reading letters

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * read_textfile - functions that reads a textfile and prints letters
+ * @filename: first argument of the function
+ * @letters: second argument of the function
+ * Reurn: returns number of letters printed and 0 when fail
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fd;
+ssize_t nrd, nwr;
+char *buf;
+if (!filename)
+{
+return (0);
+}
+fd = open(filename, 0_RDONLY);
+if (fd == -1)
+{
+return (0);
+}
+buf = malloc(sizeof(char) * (letters));
+if (!buff)
+{
+return (0);
+}
+nrd = read(fd, buf, letters);
+nwr = write(STDOUT_FILENO, buf, nrd);
+close(fd);
+free(buf);
+return (nwr);
+}
