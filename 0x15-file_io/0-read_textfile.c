fb6cafa344e21d8495e85ddaecc993eca4365a5f
Osman <oselibas@gmail.com>
2023-05-08 20:02:12 -0700

    mandatory tasks added

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * read_textfile - functions that reads a textfile and prints letters
+ * @filename: first argument of the function
+ * @letters: second argument of the function
+ * Return: returns number of letters printed and 0 when fail
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fd;
+ssize_t nrd, nwr;
+char *buf;
+if (!filename)
+{
+return (0);
+}
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+{
+return (0);
+}
+buf = malloc(sizeof(char) * (letters));
+if (!buf)
+{
+return (0);
+}
