d8fb593d7059633c01f9e8e471fc001ce9ab4e54
Osman <oselibas@gmail.com>
2023-08-03 05:23:04 -0700

    task 6 fixed

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * get_endianness -  this function checks if a machine is a little or big endian
+ * get_endianness -  function checks if a machine is a little or big endian
  * Return: returns 0 for big and 1 for little
  */
 int get_endianness(void)
